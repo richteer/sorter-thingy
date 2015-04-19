d633813e1782e398a213f8912ce15075e6467342
Eric Richter <richteer@clarkson.edu>
2015-04-19 12:33:00 -0400

    Initial parsing skeleton

---
+++ Makefile
@@ -0,0 +1,31 @@
+#Written by Chris Harding
+SHELL   = /bin/bash
+TARGET  = sorter
+CC      = clang
+CFLAGS  = -c
+LDFLAGS = -lpthread
+
+SRCS := main.c
+DEPS := $(addprefix dep/,$(patsubst %.c,%.d,$(SRCS)))
+OBJS := $(addprefix obj/,$(patsubst %.c,%.o,$(SRCS)))
+SRCS := $(addprefix src/,$(SRCS))
+
+.PHONY: all clean
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS) 
+	$(CC) $(OBJS)  $(LDFLAGS) -o $(TARGET)
+
+obj/%.o: src/%.c
+	@ mkdir -p $(@D)
+	@ mkdir -p $(subst obj,dep,$(@D))
+	$(CC) -MM -MP -MT $@ -MF $(patsubst %.o,%.d,$(subst obj,dep,$@)) $<
+	$(CC) $(CFLAGS) $< -o $@
+
+clean:
+	rm -rf obj
+	rm -rf dep
+	rm -f $(TARGET)
+
+-include $(DEPS)
---
+++ src/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char * idir;
+	char * odir;
+	int thrs;
+} args_t;
+
+
+static int parse_args(args_t * args, int argc, char ** argv)
+{
+	int i;
+	int ret = 0;
+
+	for (i = 1; i < argc; ++i) {
+		switch(argv[i][1]) {
+			case 't': args->thrs = atoi(argv[i]); break;
+			case 'd': args->idir = argv[i];       break;
+			case 'o': args->odir = argv[i];       break;
+
+			default: fprintf(stderr, "Unknown option '%s'\n", argv[i]); ret = 1; break;
+		}
+	}
+
+	if ((args->idir == NULL) || (args->odir == NULL)) {
+		fprintf(stderr, "Must specify at both an input directory (-d) and an output directory (-o)\n");
+		ret = 2;
+	} 
+	else if (!strcmp(args->idir,args->odir)) {
+		fprintf(stderr, "Input and output directory cannot be the same\n"); // Otherwise it gets messy
+		ret = 3;
+	}
+
+	return ret;
+}
+
+
+
+int main(int argc, char ** argv)
+{
+	args_t args = {0};
+	args.thrs = 1; // One thread by default
+
+	if (parse_args(&args, argc, argv)) {
+		fprintf(stderr, "Error parsing args\n");
+		return 1;
+	}
+
+	// Do the thing here
+
+	return 0;
+}
