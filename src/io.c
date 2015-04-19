493b9139e2db92e55524c7c37762aaea5ebf7c38
Eric Richter <richteer@clarkson.edu>
2015-04-19 13:02:38 -0400

    Added in file list loading info

---
+++ src/io.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+static int io_append(list_t * ls, char * name)
+{
+	if (ls->tail == NULL) {
+		ls->head = ls->tail = calloc(1,sizeof(node_t));
+	}
+	else {
+		ls->tail->next = calloc(1,sizeof(node)_t);
+		ls->tail->next->prev = ls->tail;
+		ls->tail = ls->tail->next;
+	}
+
+	strcpy(ls->tail->filename = calloc(1,strlen(name)), name);
+
+	return 0;
+}
+
+
+list_t * io_load(char * dirname)
+{
+	list_t * ls;
+	DIR * dirp;
+	struct dirent d;
+
+	ls = calloc(1, sizeof(list_t));
+
+	dirp = opendir(dirname);
+	if (dirp == NULL) {
+		fprintf(stderr, "Failed to open directory '%s'\n",dirname);
+		return NULL;
+	}
+
+	while (d = readdir(dirp)) {
+		io_append(ls, d->filename);
+	}
+
+	return ls;
+}
+
+
+node_t * io_next(list_t * ls)
+{
+	node_t * tmp;
+
+	tmp = ls->head;
+
+	if (!tmp) {
+		ls->head = ls->head->next;
+	}
+
+	return tmp;
+}
+
---
+++ src/io.h
@@ -0,0 +1,20 @@
+#ifndef _io_h_
+#define _io_h_
+
+typedef struct node_s {
+	struct node_s * next;
+	struct node_s * prev;
+	char * filename;
+}
+
+typedef struct list_s {
+	node_t * head;
+	node_t * tail;
+} list_t;
+
+list_t * io_load(char * dirname);
+node_t * io_next(list_t * ls);
+
+
+
+#endif
